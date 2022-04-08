67e98c44d621d301f2e8052709e0d678ed18e578
DESKTOP-V0FEVG4\namlp <nathan@kohyoung.com>
2022-04-08 17:34:51 +0700

    update them task va thread

--- DemoGit/DemoGit/ClassTest02.cs
+++ DemoGit/DemoGit/ClassTest02.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DemoGit
 {
     class ClassTest02
     {
+        Task<string> task;
+        Thread thread;
         public int id;
         public string name;
     }
